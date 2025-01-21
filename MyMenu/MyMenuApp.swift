f54fba23e6daaee9e2d89232ab2d5e59645c2a62
Kevin Chromik <chromik.kevin@gmail.com>
2025-01-21 10:35:54 +0100

    Initial Commit

---
+++ MyMenu/MyMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMenuApp.swift
+//  MyMenu
+//
+//  Created by Kevin Chromik on 21.01.25.
+//
+
+import SwiftUI
+
+@main
+struct MyMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
